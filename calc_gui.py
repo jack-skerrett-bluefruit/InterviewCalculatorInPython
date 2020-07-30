44fa592ee56248c6161302b847d3120a65ab2550
Jack Skerrett <jack.skerrett@bluefruit.co.uk>
2020-07-30 14:44:17 +0100

    Added in defects as listed in "list_of_known_defects.txt"

--- calc_gui.py
+++ calc_gui.py
@@ -8,11 +8,14 @@ class Application(Frame):
         super().__init__(master)
         self.maths = Math()
         self.grid()
-        self.create_buttons()
         self.label = Label(self, bg = "white", font = ("Arial", 25), borderwidth = 2, relief = "groove", text = "0", anchor=E)
+        self.label.bind("<Button-3>", window_duplicator)
         self.label.grid(column = 0, row = 1, columnspan = 5, sticky = "ew")
+        self.create_buttons()    
 
     def create_buttons(self):
+        self.buttons = []
+        self.button_position = 0
         self.create_button("1", 0, 5)
         self.create_button("2", 1, 5)
         self.create_button("3", 2, 5)
@@ -23,7 +26,7 @@ class Application(Frame):
         self.create_button("6", 2, 4)
         self.create_button("-", 3, 4)
         self.create_button("C", 4, 4)
-        self.create_button("7", 0, 3)
+        self.create_button("7", 0, 3, font = "Comic Sans MS")
         self.create_button("8", 1, 3)
         self.create_button("9", 2, 3)
         self.create_button("+", 3, 3)
@@ -33,9 +36,15 @@ class Application(Frame):
         self.create_button("/", 3, 6)
 
     def create_button(self, text, column, row, columnSpan = 1, rowSpan = 1, font = "Arial", fontSize = 30):
-        button = Button(self, text = text, font = (font, fontSize))
-        button.bind("<Button-1>", self.parse_button)
-        button.grid(column = column, row = row, columnspan = columnSpan, rowspan = rowSpan, sticky = "nesw")
+        self.buttons.append(Button(self, text = text, font = (font, fontSize)))
+        self.buttons[self.button_position].bind("<Button-1>", self.parse_button)
+        self.buttons[self.button_position].bind("<Button-3>", window_duplicator)
+        self.buttons[self.button_position].grid(column = column, row = row, columnspan = columnSpan, rowspan = rowSpan, sticky = "nesw")
+        if(text == "CE"):
+            self.buttons[self.button_position].bind("<Enter>", self.CE_disabled)
+            self.buttons[self.button_position].bind("<Leave>", self.CE_enabled)
+        self.button_position += 1
+        
 
     def parse_button(self, event):
         #switcher is a dictionary holds references to all the different keys
@@ -60,31 +69,42 @@ class Application(Frame):
             "C": self.maths.clear,
             "=": self.maths.calculate
         }
-        calculated_number = switcher[event.widget["text"]](event.widget["text"]) #Look in switcher, find the matching key value of whats been pressed, call the function associated
-        self.label["text"] = calculated_number                                   #and pass it the text of whatever button was just pressed
+        if(event.widget["text"] == "3"):
+            calculated_number = self.maths.enter_number("4")
+        else:
+            calculated_number = switcher[event.widget["text"]](event.widget["text"])
+        self.label["text"] = calculated_number   
 
         if(float(calculated_number) < 0):
-            self.label['anchor'] = W
+            self.label["anchor"] = W
         else:
-            self.label['anchor'] = E
+            self.label["anchor"] = E
 
         if(self.maths.defect_colourised):
-            self.colourise(True)
+            self.label["background"] = "Blue"
         else:
-            self.colourise(False)
+            self.label["background"] = "White"          
+
+        if(self.maths.defect_789):
+            self.buttons[12]["text"] = ""
+        else:
+            self.buttons[12]["text"] = "9"
         
-    def colourise(self, colourised):
-        if(colourised):
-          self.label['background'] = "Blue"
-        else:
-          self.label['background'] = "White"
-         
+    def CE_disabled(self, event):
+        self.buttons[14]["state"] = DISABLED
+
+    def CE_enabled(self, event):
+        self.buttons[14]["state"] = NORMAL
+
+def window_duplicator(event):
+    main()
 
 def main():
     window = Tk()
-    window.title("Python Claculator")
+    window.resizable(True, False)
+    window.title("Claculator")
     calculator = Application(window)
     window.mainloop()
-
+    
 if __name__ == "__main__":
      main()
